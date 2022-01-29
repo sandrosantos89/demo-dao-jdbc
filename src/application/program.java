a1f42d08849b490ee1de66e9686a901c79f4371b
Sandro Santos <sandro.ads.dev@gmail.com>
2022-01-28 22:10:06 -0300

    Sandro

--- src/application/program.java
+++ src/application/program.java
@@ -1,6 +1,9 @@
 package application;
 
+import java.util.Date;
+
 import model.entitites.Department;
+import model.entitites.Seller;
 
 public class program {
 
@@ -9,6 +12,10 @@ public class program {
 		Department obj = new Department(1, "Books");
 
 		System.out.println(obj);
+		
+		Seller seller = new Seller(21, "bob", "bob@gmail", new Date(), 3000.0, obj);
+		
+		System.out.println(seller);
 
 	}
 
