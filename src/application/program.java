12a53d2bc857f1f4c95c138bcbedb6eb215012f1
Sandro Santos <sandro.ads.dev@gmail.com>
2022-01-31 20:33:36 -0300

    SellerDaoJDBC

--- src/application/program.java
+++ src/application/program.java
@@ -2,6 +2,8 @@ package application;
 
 import java.util.Date;
 
+import model.dao.DaoFactory;
+import model.dao.SellerDao;
 import model.entitites.Department;
 import model.entitites.Seller;
 
@@ -12,11 +14,12 @@ public class program {
 		Department obj = new Department(1, "Books");
 
 		System.out.println(obj);
-		
+
 		Seller seller = new Seller(21, "bob", "bob@gmail", new Date(), 3000.0, obj);
+
+		SellerDao sellerDao = DaoFactory.createSellerDao();
 		
 		System.out.println(seller);
-		
 
 	}
 
---
+++ src/model/dao/impl/SellerDaoJDBC.java
@@ -0,0 +1,35 @@
+package model.dao.impl;
+
+import java.util.List;
+
+import model.dao.SellerDao;
+import model.entitites.Seller;
+
+public class SellerDaoJDBC implements SellerDao {
+
+	@Override
+	public void insert(Seller obj) {
+
+	}
+
+	@Override
+	public void update(Seller obj) {
+
+	}
+
+	@Override
+	public void deleteByID(Integer id) {
+
+	}
+
+	@Override
+	public Seller findById(Integer id) {
+		return null;
+	}
+
+	@Override
+	public List<Seller> findAll() {
+		return null;
+	}
+
+}
