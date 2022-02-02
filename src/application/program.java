92031bda7533a4782ef80ecdbfafa89541211456
Sandro Santos <sandro.ads.dev@gmail.com>
2022-02-02 20:51:10 -0300

    insert implementation

--- src/application/program.java
+++ src/application/program.java
@@ -38,6 +38,10 @@ public class program {
 			System.out.println(obj);
 		}
 
+		System.out.println("\n === Text 4: Seller isert");
+		Seller newSeller = new Seller(null, "Greg", "greg@gmail.com", new Date(), 4000.0, dep);
+		sellerDao.insert(newSeller);
+		System.out.println("Inserted! New Id  = " + newSeller.getId());
 	}
 
 }
